c05578188e0cac2c61d5d76c1601a5ad09f81b7b
Alexander <alexmayeux64@gmail.com>
2023-02-20 13:14:34 -0600

    Strangers-Things 1.1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import { fetchPost } from "../api-adapter";
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
 const Main = () => {
   const [posts, setPosts] = useState([]);
@@ -15,9 +16,19 @@ const Main = () => {
     getPosts();
   }, []);
 
+const displayPosts = posts.map ((e,idx)=> {
+  return (
+    <div key={idx}>
+      <h3>{e.title}</h3>
+      <p>{e.description}</p>
+    </div>
+  )
+})
+  
   return (
     <div id="main">
       <Navbar />
+      {displayPosts}
     </div>
   );
 };
